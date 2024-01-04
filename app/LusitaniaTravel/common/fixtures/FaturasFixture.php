b90454f0f8eb5485005b08de8c2b0d8ed69fb4b5
Maria Inês <92606230+mariainesgj@users.noreply.github.com>
2024-01-04 13:25:41 +0000

    Atualização 04/jan
    Fixtures.

---
+++ app/LusitaniaTravel/common/fixtures/FaturasFixture.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace common\fixtures;
+
+use yii\test\ActiveFixture;
+
+class FaturasFixture extends ActiveFixture
+{
+    public $modelClass = 'backend\models\Fatura';
+}
---
+++ app/LusitaniaTravel/common/fixtures/PesquisaFixture.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace common\fixtures;
+
+use yii\test\ActiveFixture;
+
+class PesquisaFixture extends ActiveFixture
+{
+    public $modelClass = 'frontend\models\PesquisaForm';
+}
+
